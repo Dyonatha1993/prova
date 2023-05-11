1bf87db6d8915c6269cbb8a31a3fbaafbfb8c4f3
Dyonatha1993 <130713868+Dyonatha1993@users.noreply.github.com>
2023-05-11 19:32:24 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+nota1 = int(input("digite a primeira nota"))
+nota2 = int(input("digite a primeira nota"))
+
+media = nota1+nota2/2
+
+if media >= 5:
+    print('aprovado')
+else:
+    print('reprovado')
